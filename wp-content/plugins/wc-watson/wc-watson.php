c8b1ffd34a3bc98533f6a479c12ba35a2cd50c4f
Johan Erasmus <johan@vine.co.za>
2017-10-23 15:50:33 +0200

    added the development environment

---
+++ wp-content/plugins/wc-watson/wc-watson.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: WooCommerce Watson Integration
+Plugin URI: http://www.webis.co.za
+Description: IBM Watson Campaign Automation
+Version: 0
+Author: Johan Erasmus
+Author URI: http://www.webis.co.za
+*/
